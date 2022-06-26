7bb93a7ad6b2fb8db9d10012d2ba933e8d3423e9
Alex Geis <siegxela@gmail.com>
2022-06-26 10:41:51 -0600

    add required webpack dependencies

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		index: "./src/index.js",
+		print: "./src/otherFunction.js",
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Todo List",
+			template: "src/template.html",
+		}),
+		new MiniCssExtractPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(s(a|c)ss)$/,
+				// use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	resolve: {
+		extensions: ["*", ".js", ".jsx"],
+		fallback: {
+			fs: false,
+			child_process: false,
+		},
+	},
+	optimization: {
+		runtimeChunk: "single",
+	},
+};
