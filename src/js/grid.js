0e5aa01ed0ac88d90dade1b0a44ed4d1ebcf0989
Alex Geis <siegxela@gmail.com>
2022-07-12 16:21:08 -0600

    move grid responsive JS starting code to separate grid.js file

---
+++ src/js/grid.js
@@ -0,0 +1,20 @@
+//TASK GRID RESPONSIVENESS
+// const task1 = document.querySelector("#task-1");
+// const task2 = document.querySelector("#task-2");
+
+// task1.addEventListener("click", function (event) {
+// 	console.log(event);
+// });
+// task2.addEventListener("click", function (event) {
+// 	console.log(event);
+// 	console.log(event.target.id);
+// 	let id1 = event.target.id;
+// 	let id2 = id1.console.log();
+// 	// squaresGrid = document.getElementsByClassName("grid-squares");
+// 	// squaresGrid[0].style.gridTemplateColumns = "1000px";
+// });
+
+// taskCardsNodeList = document.getElementsByClassName("task-card");
+// for (let i = 0; i < taskCardsNodeList.length; i++) {
+// 	taskCardsNodeList[i].style.gridColumn = "1/3";
+// }
