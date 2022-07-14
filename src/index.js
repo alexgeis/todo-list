493ac3ef557efc3738e472349543e2c3fbc559f4
Alex Geis <siegxela@gmail.com>
2022-07-13 22:44:08 -0600

    cleanup of index.js, modularize project accordion toggle display logic

--- src/index.js
+++ src/index.js
@@ -9,14 +9,13 @@ import {
 } from "./js/taskForm";
 import { domCreate } from "./js/DOM";
 import { renderTasks } from "./js/render";
+import { accordionToggle } from "./js/projectAcc";
 import "./css/style.scss";
 // import { } from "./js/";
 
 //DEFAULTS
 const DEFAULT_THEME = localStorage.getItem("theme") || "light";
 // const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-//filter example
 // const currentlyDone = savedTasks.filter((task) => task.complete === true);
 // const DEFAULT_TASKS_DONE = currentlyDone.length;
 // const DEFAULT_TASKS_TOTAL = savedBooks.length;
@@ -76,21 +75,10 @@ function setCurrentTasks(newTasks) {
 // 	currentTasksTotalCount = DEFAULT_TASKS_TOTAL;
 // }
 
-const acc = document.querySelector("#project-accordion");
-const accPanel = document.querySelector("#project-panel-display");
-acc.addEventListener("click", function () {
-	acc.classList.toggle("active");
-	if (accPanel.style.display === "block") {
-		accPanel.style.display = "none";
-	} else {
-		accPanel.style.display = "block";
-	}
-});
-
-// arg 1 = name of HTML element
-// arg 2 = class names to add
-// arg 3 = object - attributes to add -> attribute : value
-domCreate("div", ["task-card"], { "data-attribute-test": "heybuddy!" });
+//PROJECT ASIDE ACCORDIAN
+document
+	.querySelector("#project-accordion")
+	.addEventListener("click", accordionToggle);
 
 const date = new Date();
 const taskDemo1 = createTask({
@@ -126,9 +114,10 @@ document
 	.querySelector("#close-task-form")
 	.addEventListener("click", closeTaskForm);
 document.querySelector("#task-clear").addEventListener("click", clearTaskForm);
-
-const addTaskFormBtn = document.querySelector("#add-task-btn");
-addTaskFormBtn.addEventListener("click", addTaskSubmit);
+//// task submit
+document
+	.querySelector("#add-task-btn")
+	.addEventListener("click", addTaskSubmit);
 function addTaskSubmit() {
 	const newTask = createTask({
 		title: document.querySelector("#t-title").value,
@@ -150,9 +139,10 @@ document
 document
 	.querySelector("#project-clear")
 	.addEventListener("click", clearProjectForm);
-
-const addProjectFormBtn = document.querySelector("#add-project-btn");
-addProjectFormBtn.addEventListener("click", addProjectSubmit);
+////project submit
+document
+	.querySelector("#add-project-btn")
+	.addEventListener("click", addProjectSubmit);
 function addProjectSubmit() {
 	const newProject = createProject({
 		title: document.querySelector("#p-title").value,
---
+++ src/js/projectAcc.js
@@ -0,0 +1,12 @@
+const acc = document.querySelector("#project-accordion");
+const accPanel = document.querySelector("#project-panel-display");
+function accordionToggle() {
+	acc.classList.toggle("active");
+	if (accPanel.style.display === "block") {
+		accPanel.style.display = "none";
+	} else {
+		accPanel.style.display = "block";
+	}
+}
+
+export { accordionToggle };
