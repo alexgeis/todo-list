12d628eafe3fc7548488ef17cf32ae5a6674315a
Alex Geis <siegxela@gmail.com>
2022-07-17 10:38:54 -0600

    finish renderAddTaskForm() logic

--- src/js/render/addTaskForm.js
+++ src/js/render/addTaskForm.js
@@ -1,15 +1,7 @@
 import { domCreate } from "../DOM";
 
 function renderAddTaskForm() {
-	//get all tasks from all projects
-	const savedProjects = JSON.parse(localStorage.getItem("projects")) || [];
-	const totalProjectNames = [];
-	for (let i = 0; i < savedProjects.length; i++) {
-		const project = savedProjects[i];
-		totalProjectNames.push(project.title);
-	}
-
-	//form container
+	//form content
 	const formTitle = domCreate("h2", ["form_title"]);
 	formTitle.textContent = "Add new task";
 	const formSubtitle = domCreate("h2", ["form_subtitle"]);
@@ -75,66 +67,70 @@ function renderAddTaskForm() {
 		placeholder: "Notes",
 	});
 	notesLabel.appendChild(notesInput);
-
+	//project list
+	const projectListLabel = domCreate("label", [""], {
+		for: "project-task-add",
+	});
+	projectListLabel.textContent = "Add to Existing Project";
+	const projectListSelect = domCreate("select", ["form-control"], {
+		id: "project-task-add",
+		name: "project-task-add",
+	});
+	const projectListOptionDEFAULT = domCreate("option", [""], {
+		value: "null",
+		disabled: "",
+		selected: "",
+	});
+	projectListOptionDEFAULT.textContent = "--Select existing project--";
+	projectListSelect.appendChild(projectListOptionDEFAULT);
+	////  get names of all projects
+	const savedProjects = JSON.parse(localStorage.getItem("projects")) || [];
+	const totalProjectNames = [];
+	for (let i = 0; i < savedProjects.length; i++) {
+		const project = savedProjects[i];
+		totalProjectNames.push(project.title);
+	}
+	for (let i = 0; i < totalProjectNames.length; i++) {
+		const name = totalProjectNames[i];
+		const projectListOption = domCreate("option", [""], {
+			value: name,
+		});
+		projectListOption.textContent = name;
+		projectListSelect.appendChild(projectListOption);
+	}
+	const projectListErrMsg = domCreate("small", ["err-msg"]);
+	projectListErrMsg.textContent = " * Project association is required";
+	projectListLabel.append(projectListSelect, projectListErrMsg);
+	//add/clear btns
+	const taskFormBtns = domCreate("div", ["new-task-buttons"]);
+	const addBtn = domCreate("button", ["btn"], {
+		id: "add-task-btn",
+	});
+	addBtn.textContent = "Add";
+	const clearBtn = domCreate("button", ["btn"], {
+		id: "task-clear",
+	});
+	clearBtn.textContent = "Clear fields";
+	taskFormBtns.append(addBtn, clearBtn);
 	//form container
 	const formContainer = domCreate("div", ["form-container"], {
 		id: "new-project-form",
 	});
-	formContainer.append(formTitle, formSubtitle, closeBtn);
+	formContainer.append(
+		formTitle,
+		formSubtitle,
+		closeBtn,
+		titleLabel,
+		descLabel,
+		dateLabel,
+		priorityLabel,
+		notesLabel,
+		projectListLabel,
+		taskFormBtns
+	);
 	//wrapper
 	const addTaskSection = domCreate("section", ["add_task_section"]);
 	addTaskSection.appendChild(formContainer);
-
-	//update main header
-	const mainHeader = document.querySelector("#main-header");
-	mainHeader.textContent = "DASHBOARD";
-	//clear current tasks
-	const taskContainer = document.querySelector("#main-content");
-	taskContainer.innerHTML = "";
-	//render currentTasks tasks
-	for (let i = 0; i < currentTasks.length; i++) {
-		const task = currentTasks[i];
-		const taskCard = domCreate("div", ["task-card"], { "data-task-index": i });
-		//inner task card div items
-		//TITLE
-		const taskTitle = domCreate("div", ["task-title"]);
-		taskTitle.textContent = task.title;
-		//DESCRIPTION
-		const taskDesc = domCreate("div", ["task-description"]);
-		taskDesc.textContent = task.description;
-		//DUE DATE
-		const taskDate = domCreate("div", ["task-date"]);
-		taskDate.textContent = task.dueDate;
-		//PRIORITY
-		const taskPriority = domCreate("div", ["task-priority"]);
-		taskPriority.textContent = task.priority;
-		//NOTES
-		const taskNotes = domCreate("div", ["task-notes"]);
-		taskNotes.textContent = task.notes;
-		//DELETE ICON
-		const taskDeleteIcon = domCreate("img", ["task-delete"], {
-			src: deleteIcon,
-			alt: "trash can delete icon",
-		});
-		taskDeleteIcon.addEventListener("click", function () {
-			taskDeleteIcon.parentElement.remove(taskCard);
-			let index = taskDeleteIcon.parentElement.getAttribute("data-task-index");
-			currentTasks.splice(index, 1);
-			setCurrentTasks(currentTasks);
-			localStorage.setItem("tasks", JSON.stringify(currentTasks));
-			renderTasks();
-		});
-		//APPEND ELEMENTS
-		taskCard.append(
-			taskTitle,
-			taskDesc,
-			taskDate,
-			taskPriority,
-			taskNotes,
-			taskDeleteIcon
-		);
-		taskContainer.appendChild(taskCard);
-	}
 }
 
 export { renderAddTaskForm };
