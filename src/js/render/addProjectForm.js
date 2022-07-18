985de80eff22fc13ff9000514a80cea910423067
Alex Geis <siegxela@gmail.com>
2022-07-18 06:47:04 -0600

    clean up renderAddProjectForm() render logic

--- src/js/render/addProjectForm.js
+++ src/js/render/addProjectForm.js
@@ -1,1 +1,64 @@
 import { domCreate } from "../DOM";
+
+function renderAddProjectForm() {
+	//form content
+	const formTitle = domCreate("h2", ["form_title"]);
+	formTitle.textContent = "Add new task";
+	const formSubtitle = domCreate("h2", ["form_subtitle"]);
+	formSubtitle.textContent = "* required";
+	const closeBtn = domCreate("span", ["material-icons", "close-form"], {
+		id: "close-task-form",
+	});
+	closeBtn.textContent = " X ";
+	//title
+	const titleLabel = domCreate("label", [""], { for: "t-title" });
+	titleLabel.textContent = "Title *";
+	const titleInput = domCreate("input", ["form-control"], {
+		id: "t-title",
+		type: "text",
+		name: "task-title",
+		placeholder: "Title",
+	});
+	const titleErrMsg = domCreate("small", ["err-msg"]);
+	titleErrMsg.textContent = " * Task title is required";
+	titleInput.appendChild(titleErrMsg);
+	titleLabel.appendChild(titleInput);
+	//description
+	const descLabel = domCreate("label", [""], { for: "t-desc" });
+	descLabel.textContent = "Description";
+	const descInput = domCreate("input", ["form-control"], {
+		id: "t-desc",
+		type: "textarea",
+		name: "t-desc",
+		placeholder: "Description",
+	});
+	descLabel.appendChild(descInput);
+	//add/clear btns
+	const projectFormBtns = domCreate("div", ["new-task-buttons"]);
+	const addBtn = domCreate("button", ["btn"], {
+		id: "add-task-btn",
+	});
+	addBtn.textContent = "Add";
+	const clearBtn = domCreate("button", ["btn"], {
+		id: "task-clear",
+	});
+	clearBtn.textContent = "Clear fields";
+	projectFormBtns.append(addBtn, clearBtn);
+	//form container
+	const formContainer = domCreate("div", ["form-container"], {
+		id: "new-project-form",
+	});
+	formContainer.append(
+		formTitle,
+		formSubtitle,
+		closeBtn,
+		titleLabel,
+		descLabel,
+		projectFormBtns
+	);
+	//wrapper
+	const addProjectSection = domCreate("section", ["add_project_section"]);
+	addProjectSection.appendChild(formContainer);
+}
+
+export { renderAddProjectForm };
