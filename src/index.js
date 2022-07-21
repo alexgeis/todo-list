a1af857848106cf7b58e4394fa87265cf1823f58
Alex Geis <siegxela@gmail.com>
2022-07-20 18:17:19 -0600

    fresh build

--- src/index.js
+++ src/index.js
@@ -1,71 +1,29 @@
-import { createTask, createProject } from "./js/create";
-import {
-	renderTaskForm,
-	renderProjectForm,
-	clearTaskForm,
-	closeTaskForm,
-	clearProjectForm,
-	closeProjectForm,
-} from "./js/form";
-import { domCreate } from "./js/DOM";
-import { renderAddMenu, renderHeader } from "./js/render/components/header";
-import { renderDashboard } from "./js/render/pages/dashboard";
-import {
-	addTaskSubmit,
-	renderAddTaskForm,
-} from "./js/render/components/addTaskForm";
-import {
-	addProjectSubmit,
-	renderAddProjectForm,
-} from "./js/render/components/addProjectForm";
-import { setCurrentTasks, setCurrentProjects } from "./js/state";
-import { seedProjectData } from "./js/data/seed";
 import "./css/style.scss";
 // import { renderSite } from "./js/render/renderPages";
 //TEST DATA
 // import { taskData } from "./js/data/taskData";
 // localStorage.setItem("tasks", taskData);
-import { renderDashboardPage } from "./js/render/renderPages";
-
-// const content = document.getElementById("content");
-// console.log("before");
-// let el = domCreate("div", "", { id: "example" });
-// console.log("after");
-// console.log({ el });
-// el.textContent = "TEST";
-// content.appendChild(el);
-
-//SEED DATA BTN
-// document
-// 	.querySelector("#seed-projects-btn")
-// 	.addEventListener("click", seedProjectData);
+import { setCurrentTasks, setCurrentProjects } from "./js/state";
+import {
+	renderDashboardPage,
+	renderAllProjectsPage,
+} from "./js/render/renderPages";
 
 //// STATE ////
 //DEFAULTS
 const DEFAULT_THEME = localStorage.getItem("theme") || "light";
 const savedProjects = JSON.parse(localStorage.getItem("projects")) || [];
-const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 // const currentlyDone = savedTasks.filter((task) => task.complete === true);
 // const DEFAULT_TASKS_DONE = currentlyDone.length;
 // const DEFAULT_TASKS_TOTAL = savedBooks.length;
 
 let currentTasks = savedTasks;
-let currentProjects = savedProjects;
 let currentTheme = DEFAULT_THEME;
-// let currentTasksDoneCount = DEFAULT_TASKS_DONE;
-// let currentTasksTotalCount = DEFAULT_TASKS_TOTAL;
 
 window.onload = () => {
 	renderDashboardPage();
-	// renderSite();
 	// setCurrentTheme(DEFAULT_THEME);
 	// setCurrentTasks(currentTasks);
 	// setCurrentProjects(currentProjects);
-	// renderProjectAsideList();
-	// 	renderDashboard();
-	// });
-	//
-	// 	renderProjectsAll();
-	// });
-	// renderDashboard();
 };
